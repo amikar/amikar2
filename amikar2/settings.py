ff4dbc498dbf11ee37e9c05c3290d6576afab5a5
amikar <amikar@Ajays-MacBook-Pro.local>
2016-08-01 17:19:42 -0400

    23

--- amikar2/settings.py
+++ amikar2/settings.py
@@ -147,3 +147,15 @@ MEDIA_DIRS = (
 )
 
 LOGIN_URL = '/login/'
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
